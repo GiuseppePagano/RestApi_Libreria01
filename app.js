a10414d9d1b06f3e7160d8f44f7cfceb2892dcd7
Giuseppe <moddato.gp@gmail.com>
2022-05-16 09:57:46 +0200

    modifica condizione per codici uguali

--- app.js
+++ app.js
@@ -1,114 +1,116 @@
-const express = require('express')
-const bodyparser = require('body-parser')
+const express = require("express");
+const bodyparser = require("body-parser");
+
+
 
 const app = express();
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: true })) // non chiaro
 
+
+
+//TOLGO L'UGUALE
+app.use  (bodyparser.json())
+app.use  (bodyparser.urlencoded({ extended: true }))
+
+//dichiaro dove trovo le cose 
 
 const port = 3000;
 const host = "localhost";
 
-let elencoLibri = [
+//creo un oggetto che conterrebbe i nostri dati
 
+let libreria = [
     {
-
-        titolo: "Harry Potter",
-        ISBN: "123455",
-        autore: "j.k rowling"
+        isbn: "123456",
+        autore: "Maccio Capatonda",
+        titolo: "Libro"        
     },
     {
-
-        titolo: "libro cuore",
-        ISBN: "123456",
-        autore: "edmondo de amicis"
+        isbn: "654123",
+        autore: "Tre semplici passi",
+        titolo: "Fabio Volo"
+        
     },
     {
-
-        titolo: "maurizio costanzo biografia",
-        ISBN: "123457",
-        autore: "maurizio costanzo"
-    },
-
-    {
-
-        titolo: "bibbia",
-        ISBN: "123458",
-        autore: "ignoto"
+        isbn: "987123",
+        autore: "Giorgio Faletti",
+        titolo: "Io uccido"
     }
-
 ];
+//verifica dell'avvenuta connessione alla porta e all'host prima dichiarati
 
-app.listen(port, host, () => {          // 
-    console.log(`DAJE SU http://${host}:${port}/`)
+app.listen(port, host, () => {
+
+    console.log(`sono in ascolto alla porta ${port} dell' ${host}`);
 })
 
-app.get("/libri/lista", (req, res) => {
-    res.json(elencoLibri);
-})
+//se arriva una richista get (da postman) 
+app.get("/biblioteca/lista", (req, res) => {
 
+    res.json(libreria);
+});
 
-app.get("/libri/dettaglio/:ISBN", (req, res) => {       // for of da guardare
-    for (let [idx, item] of elencoLibri.entries()) {
-        if (item.ISBN == req.params.isbn) {
+app.get("/biblioteca/dettaglio/:isbn", (req,res) => {
+
+    for(let [idx,item] of libreria.entries()) {
+
+        if(res.isbn == req.params.isbn) {
             res.json(item);
         }
     }
 
-    res.json({})
+    res.json({item});
 })
 
-app.post("/libri/inserisci", (req, res) => {
 
-    let libro = {
-        titolo: req.body.tito,
-        ISBN: req.body.isbn,
-        autore: req.body.aut
-    };
+//----------------------------------------------------AGGIUNGI LIBRO ----------------------------------------
 
-    for (let [idx, item] of elencoLibri.entries()) {
-        if (item.ISBN == req.params.ISBN) {
-            res.json(
-                {
-                    status: "error"
-                })
-        }}
-        elencoLibri.push(libro);
+app.post("/biblioteca/inserisci", (req,res) => {
 
-        res.json(
-            {
-                status: "success"
+let libro = {
+            isbn : req.body.isbn,
+            autore : req.body.autore,
+            titolo : req.body.titolo
+        }
+    
+        //definisco una booleana come false
+        let bool = false;
+    
+        // se l'oggetto inserito ha un elemento (in questo caso isbn) è gia presente, non lo aggiungo nell'array ma mando messaggio d'errore
+        for (let [idx,item] of libreria.entries()){
+    
+           //se il libro inserito è gia presente, la boolenana diventa true e non aggiunge il libro in array libreria 
+    
+            if (req.body.isbn == item.isbn) {
+                bool = true
+                res.json ({Status:"error"})
             }
-        )
-    }
-)
-app.delete("/libri/dettaglio/:ISBN", (req, res) => {
-    for (let [idx, item] of elencoLibri.entries()) {
-        if (item.ISBN == req.params.ISBN) {
-            elencoLibri.splice(idx, 1)
-            res.json({ status: "success" })
+    
+            //se la booleana rimane false, il libro inserito non è presente in libreria e quindi lo aggiunge
+    
         }
-    }
+        
+        if (bool == false) {
+            libreria.push(libro)
+            res.json({status:"success"})
+        } 
+    }); 
 
-    res.json({
-        status: "error",
-        data: "item_not_found"
-    })
-})
 
-app.put("/libri/dettaglio/:ISBN", (req, res) => {
-    for (let [idx, item] of elencoLibri.entries()) {
-        if (item.ISBN == req.params.ISBN) {
+    //  ---------------------------------- ELIMINA LIBRO ---------------------------------
 
-            item.titolo = req.body.tito;
-            item.autore = req.body.aut;
 
-            res.json({ status: "success" })
+    app.delete("biblioteca/dettaglio/:isbn", (req,res) => {
+
+        for (let [idx,item] of libreria.entries()){
+            
+            if(item.isbn == req.body.isbn) {
+            libreria.splice(idx,1);
+            res.json(item);
+            }
         }
-    }
 
-    res.json({
-        status: "error",
-        data: "item_not_found"
+        res.json({
+            status:"Errore",
+            data: "Data_Not_Found"
+        })
     })
-})
